f21af27e380920f4ba9287b2b6a9efeb08ce93f9
bottleCow <lukekang94@gmail.com>
2021-07-07 14:00:16 +0900

    Spring Boot Oracle

---
+++ oracle/src/main/java/com/ch/oracle/dao/EmpDao.java
@@ -0,0 +1,14 @@
+package com.ch.oracle.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ch.oracle.model.Emp;
+
+@Mapper
+public interface EmpDao {
+
+	List<Emp> list(int deptno);
+
+}
---
+++ oracle/src/main/java/com/ch/oracle/service/EmpService.java
@@ -0,0 +1,13 @@
+package com.ch.oracle.service;
+
+import java.util.List;
+
+import com.ch.oracle.model.Emp;
+
+public interface EmpService {
+
+	List<Emp> list(int deptno);
+	
+	
+	
+}
